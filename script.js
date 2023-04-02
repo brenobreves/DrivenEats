45581c5e42d8456a449cf389ae17ce11942f4279
brenobreves <brenocsb@dcc.ufrj.br>
2023-04-01 21:09:57 -0300

    Botão de finalizar implementado

--- script.js
+++ script.js
@@ -36,5 +36,22 @@ function selecionar(p,tipo){
 }    
 
 function test(){
-        alert('Teste bem sucedido');
+    const pvcomida= document.querySelector('.food .selecionado .num');
+    const vcomida= parseFloat(pvcomida.innerHTML.replace(',' , '.'));
+    const pvbebida= document.querySelector('.drink .selecionado .num');
+    const vbebida= parseFloat(pvbebida.innerHTML.replace(',' , '.'));
+    const pvdzrt= document.querySelector('.dzrt .selecionado .num');
+    const vdzrt= parseFloat(pvdzrt.innerHTML.replace(',' , '.'));
+    const total = vcomida + vbebida + vdzrt;
+    const ftotal = total.toFixed(2);
+    const pncomida= document.querySelector('.food .selecionado .nome');
+    const ncomida= pncomida.innerHTML;
+    const pnbebida= document.querySelector('.drink .selecionado .nome');
+    const nbebida= pnbebida.innerHTML;
+    const pndzrt= document.querySelector('.dzrt .selecionado .nome');
+    const ndzrt= pndzrt.innerHTML;
+    const msg = 'Olá, gostaria de fazer o pedido\n- Prato: ' + ncomida +'\n- Bebida: ' + nbebida + '\n- Sobremesa: ' + ndzrt + '\nTotal: R$ ' + ftotal.replace('.',',');
+    const emsg = encodeURIComponent(msg);
+    const link ='https://wa.me/5521996697414?text='+ emsg;
+    window.location = link;
 }
